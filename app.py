4c69514726368f2a1868f19fb9e8b5d5bce6f200
Breno Poggiali <brenopoggiali@gmail.com>
2019-09-05 01:45:53 -0300

    Add authentication routes

--- app.py
+++ app.py
@@ -1,16 +1,91 @@
 from flask import Flask, escape, request, render_template, redirect, url_for
+from flask_login import (LoginManager, login_user, logout_user, login_required,
+            login_required, current_user, UserMixin)
 
 app = Flask(__name__)
+app.secret_key = 'super secret string'
+login_manager = LoginManager()
+login_manager.init_app(app)
+users = {'foo@bar.tld': {'password': 'secret'}}
+
+##
+
+class User(UserMixin):
+    pass
+
+
+@login_manager.user_loader
+def user_loader(email):
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+    return user
+
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+
+    # DO NOT ever store passwords in plaintext and always compare password
+    # hashes using constant-time comparison!
+    user.is_authenticated = request.form['password'] == users[email]['password']
+
+    return user
+
+##
+
 
 @app.route('/')
 def index():
   return redirect(url_for('login'))
 
-@app.route('/login', methods=('GET','POST'))
-def login():
-  return render_template('login.html')
+@app.route('/login', methods=['GET', 'POST'])
+def login(alert_auth = False):
+  if request.method == 'GET':
+    if 'alert_auth' in request.args: alert_auth = request.args['alert_auth']
+    return render_template('login.html', alert_auth=alert_auth, wrong_data=False)
+  else:
+    email = request.form['email']
+    if email in users and request.form['password'] == users[email]['password']:
+      user = User()
+      user.id = email
+      login_user(user)
+      return redirect(url_for('dashboard'))
+    else:
+      return render_template('login.html', alert_auth=True, wrong_data=True)
 
-@app.route('/register', methods=('GET', 'POST'))
+
+@app.route('/logout')
+def logout():
+  logout_user()
+  return redirect(url_for('login'))
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
   return render_template('register.html')
 
+@app.route('/dashboard')
+@login_required
+def dashboard():
+  return render_template('dashboard.html')
+
+@app.route('/pesquisar')
+@login_required
+def pesquisar():
+  return render_template('pesquisar.html')
+
+@app.route('/contribuir')
+@login_required
+def contribuir():
+  return render_template('contribuir.html')
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+  return redirect(url_for('login', alert_auth=True))

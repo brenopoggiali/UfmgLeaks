7cd843097c58feb3ccdb70a418c6966ce65b3a0b
Breno Poggiali <brenopoggiali@gmail.com>
2019-09-12 01:17:45 -0300

    Create database schema

---
+++ schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Users (
+    id_user INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    nome VARCHAR(255) NOT NULL,
+    encrypted_password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO Users ("email", "nome", "encrypted_password") VALUES
+("foo@bar.tld", "Usuário Teste", "secret");
